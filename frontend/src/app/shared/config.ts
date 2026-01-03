80b3908582323cae2a55a6bb38cff1dbe16fd432
Lucas <info@rizzagroup.com>
2026-01-03 15:06:52 +0000

    fix: update AppConfig type to make API_BASE_PROD and API_BASE_TEST optional

--- frontend/src/app/shared/config.ts
+++ frontend/src/app/shared/config.ts
@@ -1,11 +1,13 @@
 export type AppConfig = {
   API_BASE: string,
-  API_BASE_PROD: string,
+  API_BASE_PROD?: string,
+  API_BASE_TEST?: string,
 }; 
 
 const defaults: AppConfig = {
-  API_BASE: "http://localhost:3000",
+  API_BASE: "https://api.rizzagroup.com",
   API_BASE_PROD: "https://api.rizzagroup.com",
+  API_BASE_TEST: "http://localhost:3000"
 };
 
 const w = window as any;
